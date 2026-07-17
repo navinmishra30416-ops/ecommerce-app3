7db94ceb440890d8d7efe0614fc75601b1f2e1be
Navin Mishra <navimishra30416@gmail.com>
2026-07-17 12:27:02 +0530

    Log unhandled exceptions for debugging

--- ecommerce-app/src/main/java/com/ecommerce/common/exception/GlobalExceptionHandler.java
+++ ecommerce-app/src/main/java/com/ecommerce/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ecommerce.common.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +14,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleNotFound(ResourceNotFoundException ex) {
@@ -32,8 +36,9 @@ public class GlobalExceptionHandler {
         return build(HttpStatus.BAD_REQUEST, message);
     }
 
-    @ExceptionHandler(Exception.class)
+   @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneric(Exception ex) {
+        logger.error("Unhandled exception occurred", ex);
         return build(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong");
     }
 
